9b2256d19eb945f319e6de5412644e239ca91b9a
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-06-04 23:02:18 -0300

    Desafio 2 completo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
 
 import { Header } from "../components/Header";
 import { Task, TasksList } from "../components/TasksList";
@@ -9,6 +9,15 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const taskWithSameTitle = tasks.find((task) => task.title === newTaskTitle);
+
+    if (taskWithSameTitle) {
+      return Alert.alert(
+        "Task já cadastrada",
+        "Você não pode cadastrar uma task com o mesmo nome"
+      );
+    }
+
     const data = {
       id: new Date().getTime(),
       title: newTaskTitle,
@@ -33,7 +42,37 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    setTasks((oldState) => oldState.filter((task) => task.id !== id));
+    Alert.alert(
+      "Remover item",
+      "Tem certeza que você deseja remover esse item?",
+      [
+        {
+          text: "Não",
+          onPress: () => {
+            return;
+          },
+        },
+        {
+          text: "Sim",
+          onPress: () =>
+            setTasks((oldState) => oldState.filter((task) => task.id !== id)),
+        },
+      ]
+    );
+  }
+
+  function handleEditTask(taskId: number, taskNewTitle: string) {
+    const updatedTask = tasks.map((task) => ({ ...task }));
+
+    const findingTask = updatedTask.find((task) => task.id === taskId);
+
+    if (!findingTask) {
+      return;
+    }
+
+    findingTask.title = taskNewTitle;
+
+    setTasks(updatedTask);
   }
 
   return (
@@ -46,6 +85,7 @@ export function Home() {
         tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask}
+        editTask={handleEditTask}
       />
     </View>
   );
